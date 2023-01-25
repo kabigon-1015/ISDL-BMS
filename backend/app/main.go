3fc0dfd9aec4da336b169ab7290a96570408b0fb
hoka <oka.hayato@mikilab.doshisha.ac.jp>
2023-01-25 10:34:03 +0900

    seccondcommit

--- backend/app/main.go
+++ backend/app/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	//Ginの実装
 	"github.com/gin-gonic/gin"
+	"net/http"
 
-	"github.com/hoka-isdl/ISDL-BMS/backend/app/controller"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	// "github.com/hoka-isdl/ISDL-BMS/backend/app/controller"
 )
 
 func main() {
@@ -17,8 +20,18 @@ func main() {
 	router.Static("/js", "../../frontend/dist/js")
 	router.Static("/css", "../../frontend/dist/css")
 
-	//routerを渡す
-	controller.Router(router)
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"judge": true,
+		})
+	})
+
+	// //routerを渡す
+	// controller.Router(router)
 
 	//ポートを指定して実行
 	router.Run(":80")
