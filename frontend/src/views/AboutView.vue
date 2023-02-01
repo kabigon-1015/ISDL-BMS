b9611804a17068e06e2ce2a1529a5b590037c67a
unknown <sakabe.kentaro@mikilab.doshisha.ac.jp>
2023-02-01 16:26:40 +0900

    サイドメニュー固定した

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld msg="aaa"/>
   </div>
 </template>
+
+<style>
+/* ファイルパスに従って@importntの後にファイルパスを書き込む */
+@import "../assets/css/style.css";
+@import "../assets/css/print.css";
+</style>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  name: 'HelloWorld',
+  components:{
+    HelloWorld
+  },
   head: {
     title() {
       return {
