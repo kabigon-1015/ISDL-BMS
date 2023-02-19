acf00360f05a37e4d8fdb7fd750506659d921ed9
ssizawa <izawa.shin@mikilab.doshisha.ac.jp>
2023-02-19 16:17:47 +0900

    create booklist and bookdetail

--- frontend/src/views/BookList.vue
+++ frontend/src/views/BookList.vue
@@ -12,18 +12,18 @@
 			<table class="returntable">
 				<thead>
 					<tr>
-						<th>タイトル</th>
-						<th>著者</th>
-						<th>出版社</th>
-						<th>貸出状況</th>
+						<th class=title>タイトル</th>
+						<th class=author>著者</th>
+						<th class=publisher>出版社</th>
+						<th class=status>貸出状況</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr v-for="tableData in filteredbooks" :key="tableData.name">
-						<router-link :to="{name:'detail',query:{title:tableData.title}}"><td v-text="tableData.title"></td></router-link>
-						<td v-text="tableData.author"></td>
-						<td v-text="tableData.publisher"></td>
-						<td v-text="tableData.state"></td>
+						<td class=title><router-link :to="{name:'detail',query:{title:tableData.title}}" v-text="tableData.title"></router-link></td>
+						<td class=author v-text="tableData.author"></td>
+						<td class=publisher v-text="tableData.publisher"></td>
+						<td class=status v-text="tableData.state"></td>
 					</tr>
 				</tbody>
 			</table>
@@ -119,148 +119,9 @@ export default {
 </script>
 
 <style scoped>
-.cp_iptxt {
-  display: inline-block;
-  position: relative;
-  width: 30%;
-  margin: 40px 3%;
-  background-color: #b2adad;
-}
-.cp_iptxt input[type="text"] {
-  font: 15px/24px sans-serif;
-  color: #000;
-  box-sizing: border-box;
-  width: 100%;
-  letter-spacing: 1px;
-  padding-left: 4em;
-}
-.cp_iptxt input[type="text"]:focus {
-  outline: none;
-}
-.ef {
-  padding: 7px 14px;
-  transition: 0.4s;
-  border: 1px solid #1b2538;
-  background: transparent;
-}
-.ef ~ .focus_line:before,
-.ef ~ .focus_line:after {
-  position: absolute;
-  top: -1px;
-  left: 50%;
-  width: 0;
-  height: 2px;
-  content: "";
-  transition: 0.4s;
-  background-color: #da3c41;
-}
-.ef ~ .focus_line:after {
-  top: auto;
-  bottom: 0;
-}
-.ef ~ .focus_line i:before,
-.ef ~ .focus_line i:after {
-  position: absolute;
-  top: 50%;
-  left: 0;
-  width: 2px;
-  height: 0;
-  content: "";
-  transition: 0.6s;
-  background-color: #da3c41;
-}
-.ef ~ .focus_line i:after {
-  right: 0;
-  left: auto;
-}
-.ef:focus ~ .focus_line:before,
-.ef:focus ~ .focus_line:after,
-.cp_iptxt.ef ~ .focus_line:before,
-.cp_iptxt.ef ~ .focus_line:after {
-  left: 0;
-  width: 100%;
-  transition: 0.4s;
-}
-.ef:focus ~ .focus_line i:before,
-.ef:focus ~ .focus_line i:after,
-.cp_iptxt.ef ~ .focus_line i:before,
-.cp_iptxt.ef ~ .focus_line i:after {
-  top: -1px;
-  height: 100%;
-  transition: 0.6s;
-}
-.ef ~ label {
-  position: absolute;
-  font-size: 20px;
-  top: 4px;
-  left: 14px;
-  width: 5%;
-  transition: 0.3s;
-  letter-spacing: 0.5px;
-  color: #040404;
-}
-.ef:focus ~ label,
-.cp_iptxt.ef ~ label {
-  font-size: 25px;
-  top: -40px;
-  left: 0;
-  transition: 0.3s;
-  color: #fc040c;
-}
-
-*,
-*:before,
-*:after {
-  -webkit-box-sizing: inherit;
-  box-sizing: inherit;
-}
-
-html {
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  font-size: 62.5%;
-}
-
-.btn,
-a.btn,
-button.btn {
-  border: none;
-  font-size: 1rem;
-  font-weight: 200;
-  line-height: 0.7;
-  position: relative;
-  display: inline-block;
-  padding: 1rem 2rem;
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  -webkit-transition: all 0.3s;
-  transition: all 0.3s;
-  text-align: center;
-  vertical-align: middle;
-  text-decoration: none;
-  letter-spacing: 0.1em;
-  color: #212529;
-  border-radius: 0.5rem;
-}
-
-button.btn--yellow {
-  color: #000;
-  background-color: #00d0ff;
-  border-bottom: 5px solid #0096cc;
-}
-
-button.btn--yellow:hover {
-  margin-top: 3px;
-  color: #000;
-  background: #00d0ff;
-  border-bottom: 2px solid #0096cc;
-}
 table {
   position: absolute;
-  table-layout: fixed;
+  /* table-layout: fixed; */
   /* top: 130px; */
   size: 200px;
   border-collapse: collapse;
@@ -277,26 +138,49 @@ table tr {
 /* thead */
 thead th {
 	font-size: 12px;
-	/* width: 100px; */
+	/* width: 300px; */
 	text-align: center;
-	padding: 8px 0;
+	padding: 8px;
 	/* color */
 	color: white;
-	background-color:#1b2538;
+	background-color:#b3ae92;
 	border-top: 1px solid #a2a7af;
 	border-left: 1px solid #a2a7af;
 	border-right: 1px solid #a2a7af;
 	border-bottom:3px solid #a2a7af;
 }
-/* tbody */
-tbody {
-	overflow-y: scroll;
+a {
+	color: #111;		/*リンクテキストの色*/
+	transition: 0.4s;	/*マウスオン時の移り変わるまでの時間設定。0.4秒。*/
+	text-decoration: none;
 }
-.tbody::-webkit-scrollbar {  /* Chrome, Safari 対応 */
-        display:none;
-    }
+a:hover {
+	color: #448db3;			/*マウスオン時の文字色*/
+	text-decoration: none;	/*マウスオン時に下線を消す設定。残したいならこの１行削除。*/
+}
+/* tbody */
+thead{
+  display: block;
+}
+tbody {
+  display: block;
+  overflow-y: scroll;
+  height: 380px;
+}
+.title{
+  min-width: 250px;
+}
+.author{
+  min-width: 250px;
+}
+.publisher{
+  min-width: 250px;
+}
+.status{
+  width: 99999px;
+}
 tbody td {
-	/* width: 30%; */
+	/* width: 300px; */
 	text-align: center;
 	padding: 8px;
 	/* color */
@@ -319,9 +203,6 @@ input[type="checkbox"] + label {
   font: 14px/20px 'Open Sans', Arial, sans-serif;
   color: #ddd;
   cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
 }
 
 input[type="checkbox"] + label:last-child { margin-bottom: 0; }
